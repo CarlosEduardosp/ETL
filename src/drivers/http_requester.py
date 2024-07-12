9fbd13f1441f65a3233eac865669a68721d7659b
Carlos Eduardosp <carlos.spadilha@yahoo.com.br>
2024-07-11 23:30:01 -0300

    feat: criando class requests

---
+++ src/drivers/http_requester.py
@@ -0,0 +1,30 @@
+"""
+Módulo para realizar o GET dos dados.
+"""
+
+from typing import Dict
+import requests
+
+
+class HttpRequester:
+    """
+    Classe HttpRequester.
+    """
+
+    def __init__(self) -> None:
+        """
+        Método construtor com a URL da requisição.
+        """
+        self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
+
+    def request_from_page(self) -> Dict:
+        """
+        Realiza uma requisição GET na URL desejada e retorna um dicionário com os dados.
+
+        :return: Dicionário com dados do GET na URL desejada.
+        """
+        response = requests.get(self.__url, timeout=10)
+        return {
+            "status_code": response.status_code,
+            "html": response.text
+        }
---
+++ src/drivers/http_requester_test.py
@@ -0,0 +1,25 @@
+"""
+arquivo de teste
+"""
+from requests_mock import Mocker
+from .http_requester import HttpRequester
+
+
+def test_request_from_page(requests_mock: Mocker) -> None:
+    """
+    Testa o método request_from_page da classe HttpRequester.
+
+    :param requests_mock: modulo request_mock
+
+    """
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
+    response_context = '<h1>Ola Mundo</h1>'
+    requests_mock.get(url, status_code=200, text=response_context )
+
+    http_request = HttpRequester()
+    request_response = http_request.request_from_page()
+
+    assert 'status_code' in request_response
+    assert 'html' in request_response
+    assert request_response['status_code'] == 200
+    assert request_response['html'] == response_context
